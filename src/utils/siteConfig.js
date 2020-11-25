06806d1f5f7f24f43f9305abc2479c2de2d5bf78
kevindstevens1010 <67278603+kevindstevens1010@users.noreply.github.com>
2020-11-25 16:36:03 -0600

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://electrifiedventures.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `http://stoic-volhard-98e7df.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
